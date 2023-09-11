da4a88e8d8d0c46ced0f9589f5b318242f55366a
RonoBeatrice1 <ronobc24@gmail.com>
2023-09-11 21:58:04 +0300

    I've added comments to explain the purpose of the variables, functions, and event listeners, making the code more maintainable.

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ var faq = document.getElementsByClassName("faq-page");
 
 var i = 0;
 for (let i = 0; i < faq.length; i++) {
+  // This for loop iterates through all the elements with the "faq-page" class.
   faq[i].addEventListener("click", function () {
-    this.classList.toggle("toggle");
+    //This line adds a click event listener to each FAQ element in the loop
+    this.classList.toggle("toggle"); //f the element has the "toggle" class, it will be removed; if it doesn't have the "toggle" class, it will be added. This is typically used for changing the appearance or style of the FAQ when it's clicked to indicate that it's open or closed.
 
-    var body = this.nextElementSibling;
+    var body = this.nextElementSibling; //This line selects the next sibling of the clicked FAQ element, which is assumed to be the content (the answer) associated with the FAQ question. It stores this element in the body variable.
     if (body.style.display === "block") {
       body.style.display = "none";
     } else {
